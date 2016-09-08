0cc1cf76660631ed3dd6cf9915afc8875b28e54a
Rinat Muhamedgaliev <rinat.muhamedgaliev@gmail.com>
2016-09-08 16:31:49 +0300

    Fixed location

--- www/geolocation.js
+++ www/geolocation.js
@@ -80,6 +80,8 @@ var geolocation = {
         argscheck.checkArgs('fFO', 'geolocation.getCurrentPosition', arguments);
         options = parseParameters(options);
 
+        exec(successCallback, errorCallback, "Geolocation", "getCurrentPosition", []);
+
         // Timer var that will fire an error callback if no position is retrieved from native
         // before the "timeout" param provided expires
         var timeoutTimer = {timer:null};
@@ -116,31 +118,25 @@ var geolocation = {
             }
         };
 
-        // Check our cached position, if its timestamp difference with current time is less than the maximumAge, then just
-        // fire the success callback with the cached position.
-        if (geolocation.lastPosition && options.maximumAge && (((new Date()).getTime() - geolocation.lastPosition.timestamp) <= options.maximumAge)) {
-            successCallback(geolocation.lastPosition);
-        // If the cached position check failed and the timeout was set to 0, error out with a TIMEOUT error object.
-        } else if (options.timeout === 0) {
-            fail({
-                code:PositionError.TIMEOUT,
-                message:"timeout value in PositionOptions set to 0 and no cached Position object available, or cached Position object's age exceeds provided PositionOptions' maximumAge parameter."
-            });
-        // Otherwise we have to call into native to retrieve a position.
-        } else {
-            if (options.timeout !== Infinity) {
-                // If the timeout value was not set to Infinity (default), then
-                // set up a timeout function that will fire the error callback
-                // if no successful position was retrieved before timeout expired.
-                timeoutTimer.timer = createTimeout(fail, options.timeout);
-            } else {
-                // This is here so the check in the win function doesn't mess stuff up
-                // may seem weird but this guarantees timeoutTimer is
-                // always truthy before we call into native
-                timeoutTimer.timer = true;
-            }
-            exec(win, fail, "Geolocation", "getLocation", [options.enableHighAccuracy, options.maximumAge]);
-        }
+        exec(successCallback, errorCallback, "Geolocation", "getCurrentPosition", []);
+
+//        if (geolocation.lastPosition && options.maximumAge && (((new Date()).getTime() - geolocation.lastPosition.timestamp) <= options.maximumAge)) {
+//            successCallback(geolocation.lastPosition);
+//            exec(successCallback, errorCallback, "Geolocation", "getCurrentPosition", []);
+//        } else if (options.timeout === 0) {
+//            fail({
+//                code:PositionError.TIMEOUT,
+//                message:"timeout value in PositionOptions set to 0 and no cached Position object available, or cached Position object's age exceeds provided PositionOptions' maximumAge parameter."
+//            });
+//
+//        } else {
+//            if (options.timeout !== Infinity) {
+//                timeoutTimer.timer = createTimeout(fail, options.timeout);
+//            } else {
+//                timeoutTimer.timer = true;
+//            }
+//            exec(win, fail, "Geolocation", "getLocation", [options.enableHighAccuracy, options.maximumAge]);
+//        }
         return timeoutTimer;
     },
     /**
